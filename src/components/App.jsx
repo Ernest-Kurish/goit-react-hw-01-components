783902de2c9ff150588fd0c4ce78c29576162f1a
Ernest-Kurish <kurish.ernest@gmail.com>
2023-05-21 12:42:30 +0900

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { ProfileCard } from "./Profile/ProfileCard";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import user from 'data/user'
+import data from 'data/data'
+import friends from 'data/friends'
+import transactions from 'data/transactions'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h2>1 - Профіль соціальної мережі</h2>
+      <ProfileCard user={user} />
+      <h2>2- Секція статистики</h2>
+      <Statistics title="Upload stats" data={data} />
+      <h2>3 - Список друзів</h2>
+      <FriendList friends={friends} />
+      <h2>4 - Історія транзакцій</h2>
+      <TransactionHistory transactions={transactions} />
+    </>
   );
 };
